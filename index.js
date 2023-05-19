eb7e838cd529d96128def042c444a458b8bc23cc
developersharif1919 <developersharif1919@gmail.com>
2023-05-19 11:05:35 +0600

    Insert Toy Information Data In Mongodb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware 
 app.use(cors());
@@ -28,12 +30,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('magicalDreamsToys').collection('toys');
+
+    app.get('/toys', async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/toys', async (req, res) => {
+      const toyInformation = req.body;
+
+      // Generate a new ID for the subcategory
+  const subcategoryId = uuidv4();
+
+       // Assign the ID to the subcategory object
+  toyInformation.category.subcategories[0].id = subcategoryId;
+
+      const result = await toysCollection.insertOne(toyInformation);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -46,10 +70,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Magical Dreams Toys Is Running');
+app.get('/', (req, res) => {
+  res.send('Magical Dreams Toys Is Running');
 })
 
-app.listen(port, ()=>{
-    console.log(`Magical Dreams Toys Is Running On: ${port}`);
+app.listen(port, () => {
+  console.log(`Magical Dreams Toys Is Running On: ${port}`);
 })
